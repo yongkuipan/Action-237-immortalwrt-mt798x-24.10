0fb04cc95d4d22ca901a190b5a2327e7edeb02c5
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2025-03-20 09:44:06 +0800

    Update 2410-diy-part1-pw.sh

--- 2410-diy-part1-pw.sh
+++ 2410-diy-part1-pw.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '1i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
