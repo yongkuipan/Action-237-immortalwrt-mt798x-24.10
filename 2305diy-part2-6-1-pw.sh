fdd7066763e07fe04c4bdc894939f009e6e664eb
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2025-04-06 10:02:20 +0800

    Create 2305diy-part2-6-1-pw.sh

---
+++ 2305diy-part2-6-1-pw.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/ImmortalWrt/ImmortalWrt-Hanwckf/g' package/base-files/files/bin/config_generate
+
+# Modify filename, add date prefix
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-pw-23.05/1' include/image.mk
+
+# Modify ppp-down, add sleep 3. my source code is change, no need this
+sed -i '$a\\sleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
+
+# Modify opkg url, change mt7981 to filogic
+sed -i.bak "s,mt7981,filogic,g" "/etc/opkg/distfeeds.conf"
+
+# Modify opkg url, del passwall
+sed -i.bak '/passwall/d' "/etc/opkg/distfeeds.conf"
