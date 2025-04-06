c28fff676e4e0e15b1162a6cb7aa1ccf8e8cdbbc
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2025-04-06 12:24:29 +0800

    Update 2305-diy-part2-6-1-pw.sh

--- 2305-diy-part2-6-1-pw.sh
+++ 2305-diy-part2-6-1-pw.sh
@@ -24,3 +24,6 @@ sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-PW
 
 # Modify ppp-down, add sleep 3. my source code is change, no need this
 sed -i '$a\\sleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
+
+# Insert two lines before the last line in 99-default-settings-chinese. For Modify opkg url, change mt7981 to filogic, del lines inlude passwall.
+sed -i '/^exit 0$/i sed -i "s,mt7981,filogic,g" "package/base-files/files/etc/opkg/distfeeds.conf"\nsed -i '\''/passwall/d'\'' "package/base-files/files/etc/opkg/distfeeds.conf"' package/emortal/default-settings/files/99-default-settings-chinese
