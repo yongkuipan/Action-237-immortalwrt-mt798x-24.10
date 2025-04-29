38422802933dc6631dd625ce7e34b9a7d39cfb6c
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2025-04-29 12:11:36 +0800

    Update 2410-diy-part2-6-1-pw.sh

--- 2410-diy-part2-6-1-pw.sh
+++ 2410-diy-part2-6-1-pw.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/ImmortalWrt/ImmortalWrt-24.10-$(date +%Y-%m-%d)/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/ImmortalWrt-24.10-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
 sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-PW-24.10|' include/image.mk
