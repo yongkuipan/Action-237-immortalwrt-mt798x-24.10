a97baf7dce387b11e93a3e3ed1015e9b7a6cab8f
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2025-07-09 11:08:12 +0800

    Update 2410-6.6-diy-part2-6-1-pw.sh

--- 2410-6.6-diy-part2-6-1-pw.sh
+++ 2410-6.6-diy-part2-6-1-pw.sh
@@ -20,7 +20,7 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i 's/ImmortalWrt/ImmortalWrt-24.10-6.6-PW-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
-sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-PW-24.10-6.6|' include/image.mk
+sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-PW-24.10-6.6-|' include/image.mk
 
 # Modify ppp-down, add sleep 3. 2025-6-13 source code is update, no need this
 #sed -i '$a\\sleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
