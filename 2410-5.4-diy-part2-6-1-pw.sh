f3bfcaeaa69f9f1cefb0589112a50bec362f8bb7
ATang007ZH <132867229+ATang007ZH@users.noreply.github.com>
2025-07-09 11:08:27 +0800

    Update and rename 2410-diy-part2-6-1-pw.sh to 2410-5.4-diy-part2-6-1-pw.sh

--- 2410-5.4-diy-part2-6-1-pw.sh
+++ 2410-5.4-diy-part2-6-1-pw.sh
@@ -17,10 +17,10 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/ImmortalWrt/ImmortalWrt-24.10-PW-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/ImmortalWrt-24.10-5.4-PW-$(shell TZ="Asia/Shanghai" date +"%Y%m%d")/g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
-sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-PW-24.10|' include/image.mk
+sed -i 's|IMG_PREFIX:=|IMG_PREFIX:=$(shell TZ="Asia/Shanghai" date +"%Y%m%d")-PW-24.10-5.4-|' include/image.mk
 
 # Modify ppp-down, add sleep 3. 2025-6-13 source code is update, no need this
 #sed -i '$a\\sleep 3' package/network/services/ppp/files/lib/netifd/ppp-down
